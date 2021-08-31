305096935855b0e78451ebf25ab575e14b27256c
JinYong Kim <jinyong@jadekim.kr>
2021-08-31 16:18:06 +0900

    Add schema

--- bin/generate_schema.sh
+++ bin/generate_schema.sh
@@ -3,8 +3,9 @@
 SCRIPT_PATH=$(dirname "$0")
 
 cd "$SCRIPT_PATH/../contracts/campaign" && cargo schema
-cd "$SCRIPT_PATH/../contracts/campaign-manager" && cargo schema
+cd "$SCRIPT_PATH/../contracts/campaign_manager" && cargo schema
 cd "$SCRIPT_PATH/../contracts/fund_manager" && cargo schema
 cd "$SCRIPT_PATH/../contracts/governance" && cargo schema
 cd "$SCRIPT_PATH/../contracts/lp_staking" && cargo schema
 cd "$SCRIPT_PATH/../contracts/qualifier_base" && cargo schema
+cd "$SCRIPT_PATH/../packages/valkyrie_qualifier" && cargo schema
