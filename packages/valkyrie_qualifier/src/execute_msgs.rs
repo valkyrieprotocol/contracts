fa090b8721d6c9cee026809deed17061433dd62a
JinYong Kim <jinyong@jadekim.kr>
2021-10-04 14:15:41 +0900

    Release v1.0.1

--- packages/valkyrie_qualifier/src/execute_msgs.rs
+++ packages/valkyrie_qualifier/src/execute_msgs.rs
@@ -6,5 +6,6 @@ use crate::QualificationMsg;
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    Execute {},
     Qualify(QualificationMsg),
 }
