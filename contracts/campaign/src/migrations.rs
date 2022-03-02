2eb00e562d261b994ace583865c706bcfca032dc
FelixKim <felix@buzlink.io>
2022-03-02 14:24:03 +0900

    restore migration code

--- contracts/campaign/src/migrations.rs
+++ contracts/campaign/src/migrations.rs
@@ -13,11 +13,16 @@ use serde::{Deserialize, Serialize};
 
 pub fn migrate(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     msg: MigrateMsg,
 ) -> ContractResult<Response> {
     // Execute
-    let response = make_response("migrate");
+    let mut response = make_response("migrate");
+
+    response = response.add_attribute("chain_id", env.block.chain_id.clone());
+    let mut campaign_state = CampaignState::load(deps.storage)?;
+    campaign_state.chain_id = env.block.chain_id;
+    campaign_state.save(deps.storage)?;
 
     validate_participation_reward_distribution_schedule(&msg.participation_reward_distribution_schedule)?;
 
